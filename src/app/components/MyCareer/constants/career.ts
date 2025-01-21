743983ee270b4aeaedb0aeeb4df1df0245a0457d
Oleg-Ivnv <oleg.ivanov@waites.net>
2025-01-21 07:52:11 +0200

    chore: text changes

--- src/app/components/MyCareer/constants/career.ts
+++ src/app/components/MyCareer/constants/career.ts
@@ -7,20 +7,20 @@ export const CAREER = [
   },
   {
     description:
-      "As a Frontend Developer at Lumitech, you'll be at the forefront of crafting intuitive and visually stunning web applications. You'll collaborate closely with designers, backend developers, and stakeholders to deliver high-quality, responsive interfaces that enhance user experiences. Leveraging modern technologies, frameworks, and best practices, you'll contribute to building scalable and maintainable codebases that reflect Waites' commitment to innovation and excellence. Your work will be pivotal in driving the company's vision forward by ensuring seamless interaction and functionality for our users.",
+      "As a developer, I was responsible for building mobile applications using React Native. My duties included overseeing the full deployment process and ensuring successful app releases. I collaborated closely with the design team to implement UI/UX designs and ensure a seamless user experience. Additionally, I communicated regularly with clients to present progress, showcase completed work, and ensure their expectations were met throughout the project.",
     companyName: "Lumitech (Part-Time)",
     timeOfWork: "2024 - Now",
   },
   {
     description:
-      "As a Frontend Developer at AAT, you'll be at the forefront of crafting intuitive and visually stunning web applications. You'll collaborate closely with designers, backend developers, and stakeholders to deliver high-quality, responsive interfaces that enhance user experiences. Leveraging modern technologies, frameworks, and best practices, you'll contribute to building scalable and maintainable codebases that reflect Waites' commitment to innovation and excellence. Your work will be pivotal in driving the company's vision forward by ensuring seamless interaction and functionality for our users.",
+      "As a back-end developer, I was responsible for ensuring the smooth integration of various systems, including working with Amazon API to enhance the functionality of the application. My role involved maintaining efficient communication with clients to understand their needs and provide regular updates on project progress. I handled all aspects of back-end development, from managing server-side logic to troubleshooting and optimizing performance, ensuring the application’s robustness and scalability.",
     companyName: "AAT (Part-Time)",
     timeOfWork: "2023 - 2023",
   },
   {
-    description:
-      "As a Frontend Developer at AAT, you'll be at the forefront of crafting intuitive and visually stunning web applications. You'll collaborate closely with designers, backend developers, and stakeholders to deliver high-quality, responsive interfaces that enhance user experiences. Leveraging modern technologies, frameworks, and best practices, you'll contribute to building scalable and maintainable codebases that reflect Waites' commitment to innovation and excellence. Your work will be pivotal in driving the company's vision forward by ensuring seamless interaction and functionality for our users.",
     companyName: "Freelance (Part-Time)",
+    description:
+      "As a freelancer, I was responsible for managing client communications, understanding their needs, and delivering tailored solutions. I created landing pages, designed custom websites, and built e-commerce platforms with seamless WordPress integrations. My work also included custom design creation, ensuring that each project met the client's vision and business goals. I handled all aspects of web development, from design to deployment, ensuring high-quality, user-friendly sites.",
     timeOfWork: "2021 - 2022",
   },
 ];
